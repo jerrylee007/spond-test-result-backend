873d00d1733deb2338aec88939e2085f6867c105
jerry <16907478@qq.com>
2019-06-12 17:50:20 +0800

    Updated

---
+++ createDir.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir screenshots
+mkdir screenshots/android_dev
+mkdir screenshots/android_dev/backup
+mkdir screenshots/android_dev/base
+mkdir screenshots/android_dev/result
+mkdir screenshots/android_dev/new
+mkdir screenshots/android_staging
+mkdir screenshots/android_staging/backup
+mkdir screenshots/android_staging/base
+mkdir screenshots/android_staging/result
+mkdir screenshots/android_staging/new
+mkdir screenshots/ios_dev/
+mkdir screenshots/ios_dev//backup
+mkdir screenshots/ios_dev//base
+mkdir screenshots/ios_dev//result
+mkdir screenshots/ios_dev/new
+mkdir screenshots/ios_staging
+mkdir screenshots/ios_staging/backup
+mkdir screenshots/ios_staging/base
+mkdir screenshots/ios_staging/result
+mkdir screenshots/ios_staging/new
+mkdir screenshots/web_dev
+mkdir screenshots/web_dev/backup
+mkdir screenshots/web_dev/base
+mkdir screenshots/web_dev/result
+mkdir screenshots/web_dev/new
+mkdir screenshots/web_staging
+mkdir screenshots/web_staging/backup
+mkdir screenshots/web_staging/base
+mkdir screenshots/web_staging/result
+mkdir screenshots/web_staging/new
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -i ~/.ssh/tencent_20180605 server.js root@118.24.41.169:/mnt/vdb/spond-test-results-backend/
+scp -i ~/.ssh/jenkins_20190401 server.js root@118.24.41.169:/mnt/vdb/spond-test-results-backend/
